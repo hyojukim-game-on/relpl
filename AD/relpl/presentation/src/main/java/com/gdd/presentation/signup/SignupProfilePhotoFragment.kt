effe0438876459259ebda239df852045a68d1a28
홍유준 <ghddbwns9808@gmail.com>
2024-01-23 17:51:26 +0900

    feat: 갤러리 권한 추가 및 사진 불러오기 기능
    Change-Id: I26f72503d8f620f8cedf497437e31fd3edf15414

--- AD/relpl/presentation/src/main/java/com/gdd/presentation/signup/SignupProfilePhotoFragment.kt
+++ AD/relpl/presentation/src/main/java/com/gdd/presentation/signup/SignupProfilePhotoFragment.kt
@@ -1,18 +1,32 @@
 package com.gdd.presentation.signup
 
 import android.annotation.SuppressLint
+import android.app.Activity.RESULT_OK
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.gdd.presentation.R
 import com.gdd.presentation.SignupActivity
 import com.gdd.presentation.base.BaseFragment
 import com.gdd.presentation.databinding.FragmentSignupProfilePhotoBinding
+import java.io.File
+import java.util.jar.Manifest
 import kotlin.math.log
 
 private const val TAG = "SignupProfilePhotoFragm_Genseong"
@@ -21,7 +35,34 @@ class SignupProfilePhotoFragment : BaseFragment<FragmentSignupProfilePhotoBindin
 ) {
     private lateinit var signUpActivity: SignupActivity
     private val activityViewModel: SignupViewModel by activityViewModels()
+    private lateinit var profilePhotoFile: File
+    private val galleryResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
+        if (result.resultCode == RESULT_OK){
+            val imgUri = result.data?.data
+            imgUri.let {
+                profilePhotoFile = File(getRealPathFromURI(it!!))
+                Glide.with(this)
+                    .load(imgUri)
+                    .fitCenter()
+                    .apply(RequestOptions().circleCrop())
+                    .into(binding.ivProfilePhoto)
 
+            }
+        }
+    }
+
+    private val galleryPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            if (it){
+                val intent = Intent(Intent.ACTION_PICK)
+                intent.setDataAndType(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    "image/*"
+                )
+                galleryResult.launch(intent)
+            }else
+                Log.d(TAG, "deny")
+        }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -38,12 +79,34 @@ class SignupProfilePhotoFragment : BaseFragment<FragmentSignupProfilePhotoBindin
     }
 
     private fun registerListener(){
-        binding.btnAddPhoto.setOnClickListener {
-            Log.d(TAG, "registerListener: ${signUpActivity.nickname}")
+        binding.ivProfilePhoto.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+                galleryPermissionLauncher.launch(android.Manifest.permission.READ_MEDIA_IMAGES)
+            else
+                galleryPermissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
         }
     }
 
     private fun registerObserver(){
 
+    }
+
+    private fun getRealPathFromURI(uri: Uri): String{
+        val buildName = Build.MANUFACTURER
+        if (buildName == "Xiomi") return uri.path!!
+
+        var columnIndex = 0
+        val proj = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor = signUpActivity.contentResolver.query(uri, proj, null, null, null)
+        if (cursor!!.moveToFirst()){
+            columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        }
+        val result = cursor.getString(columnIndex)
+        cursor.close()
+        return result
+    }
+
+    companion object{
+        const val REQ_GALLERY = 10001
     }
 }
