6cf1438a154bfae380dd3393b00b6ed8eca41b3e
DESKTOP-LL832F4\SSAFY <tig04204@naver.com>
2024-01-23 09:14:56 +0900

    feat: 코인 (포인트내역과 포인트바코드. service class, repository는 미완성)
    body

--- BE/relpl/src/main/java/com/ssafy/relpl/service/CoinbarcodeService.java
+++ BE/relpl/src/main/java/com/ssafy/relpl/service/CoinbarcodeService.java
@@ -1,11 +1,17 @@
 package com.ssafy.relpl.service;
 
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class CoinbarcodeService {
-    
+
+    private static final int LOCATION_FLAG_DEDUCTION_POINTS = 100;
+
+    // 위치 플래그 제보 시 포인트 차감
+    public void reportLocationFlag() {
+        log.info("위치 플래그 제보 시 포인트 {}point 차감", LOCATION_FLAG_DEDUCTION_POINTS);
+        // 여기 포인트 차감 로직을 추가
+    }
 }
