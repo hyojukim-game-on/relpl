2633bbfca83df6687b00b641fed3792afdc7d949
songminseok <tig04204@naver.com>
2024-01-23 14:41:52 +0900

    Reapply "Reapply "feat: 코인 (포인트내역과 포인트바코드. service class, repository는 미완성)""
    This reverts commit 8dcb4baf718e605afcb03b38d574638b5ae2b570.

--- BE/relpl/src/main/java/com/ssafy/relpl/service/CoinService.java
+++ BE/relpl/src/main/java/com/ssafy/relpl/service/CoinService.java
@@ -7,5 +7,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class CoinService {
 
+    private static final int PROJECT_COMPLETION_POINTS = 100;
+    private static final int PLOGGING_COMPLETION_POINTS = 10;
 
+    // 프로젝트 완료 시 포인트 지급
+    public void completeProject() {
+        log.info("프로젝트 완료 시 포인트 {}point 지급", PROJECT_COMPLETION_POINTS);
+        // 여기 포인트 지급 로직을 추가
+    }
+
+    // 플로깅 완료 시 포인트 지급
+    public void completePlogging() {
+        log.info("100미터 플로깅 완료 시 포인트 {}point 지급", PLOGGING_COMPLETION_POINTS);
+        // 여기 포인트 지급 로직을 추가
+    }
 }
