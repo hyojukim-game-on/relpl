2537858d4a94aecf90c460e56fc7593bb7c58333
ksh7412 <ksh7412.dev@gmail.com>
2024-01-23 16:18:35 +0900

    Revert "feat : 테스트 추가"
    This reverts commit 011a24a85048758223437c1f910870d38af55176.

--- BE/relpl/src/main/java/com/ssafy/relpl/service/UserService.java
+++ BE/relpl/src/main/java/com/ssafy/relpl/service/UserService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 public class UserService {
     @Autowired
     UserRepository userRepository;
-// test 
+
 //    public String selectUser(String name) {
 //        ObjectMapper objectMapper = new ObjectMapper();
 //    }
