0b1080c5f3a03b8b5894d4e62d7e5ab127139561
cjfwn96 <cjfwn96@naver.com>
2024-01-25 16:28:22 +0900

    feat: PrefManager 구현
    Change-Id: Ibdcb9e77517a63c4cd629c4ddb46f5950eeef4fc

---
+++ AD/relpl/presentation/src/main/java/com/gdd/presentation/PrefManager.kt
@@ -0,0 +1,64 @@
+package com.gdd.presentation
+
+import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
+
+/**
+ * Only Use By Hilt Injection
+ *
+ * 반드시 Hilt 모듈의 @Inject로 주입해 사용하세요
+ */
+class PrefManager(context: Context) {
+    private var pref: SharedPreferences =
+        context.getSharedPreferences("relpl_preference", Activity.MODE_PRIVATE)
+
+    /**
+     * @param id
+     *
+     * id is not idInput but server user index id
+     *
+     * 유저의 입력값이 아닌 서버의 유저 인덱스 입니다.
+     */
+    fun setUserId(id: Long){
+        pref.edit().apply {
+            putLong(PREF_ACCESSTOKEN, id)
+            apply()
+        }
+    }
+
+    /**
+     * @return if there is no id return -1
+     */
+    fun getUserId(): Long{
+        return pref.getLong(PREF_USERID,-1)
+    }
+
+    fun setAccessToken(accessToken: String){
+        pref.edit().apply {
+            putString(PREF_ACCESSTOKEN, accessToken)
+            apply()
+        }
+    }
+
+    fun getAccessToken(): String? {
+        return pref.getString(PREF_ACCESSTOKEN, null)
+    }
+
+    fun setRefreshToken(refreshToken: String){
+        pref.edit().apply {
+            putString(PREF_REFRESHTOKEN, refreshToken)
+            apply()
+        }
+    }
+
+    fun getRefreshToken(): String? {
+        return pref.getString(PREF_REFRESHTOKEN, null)
+    }
+
+    companion object{
+        private const val PREF_USERID = "user_id"
+        private const val PREF_ACCESSTOKEN = "access_token"
+        private const val PREF_REFRESHTOKEN = "refresh_token"
+    }
+}
