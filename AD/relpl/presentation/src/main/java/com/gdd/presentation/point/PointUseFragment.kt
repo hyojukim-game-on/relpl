6ce9ee158290bc4cecb1a4a59167e92f31c20441
정철주 <cjfwn96@naver.com>
2024-01-26 17:49:23 +0900

    create: 포인트 사용 프래그먼트 생성
    Change-Id: Idab7b8f9833a255e440728dbd279ae513e309c0c

---
+++ AD/relpl/presentation/src/main/java/com/gdd/presentation/point/PointUseFragment.kt
@@ -0,0 +1,21 @@
+package com.gdd.presentation.point
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gdd.presentation.R
+import com.gdd.presentation.base.BaseFragment
+import com.gdd.presentation.databinding.FragmentPointUseBinding
+
+class PointUseFragment : BaseFragment<FragmentPointUseBinding>(
+    FragmentPointUseBinding::bind, R.layout.fragment_point_use
+) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+    }
+
+}
