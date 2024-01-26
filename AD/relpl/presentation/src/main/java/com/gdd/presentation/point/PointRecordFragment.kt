2f1f4fff3437e921adbbd632267d9f9b9645427f
정철주 <cjfwn96@naver.com>
2024-01-26 16:09:37 +0900

    design: 포인트 내역 프래그먼트 디자인
    Change-Id: I0bd3974e48a515c0af5c42513b04c96eb879cc9c

---
+++ AD/relpl/presentation/src/main/java/com/gdd/presentation/point/PointRecordFragment.kt
@@ -0,0 +1,21 @@
+package com.gdd.presentation.point
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gdd.presentation.R
+import com.gdd.presentation.base.BaseFragment
+import com.gdd.presentation.databinding.FragmentPointRecordBinding
+
+class PointRecordFragment : BaseFragment<FragmentPointRecordBinding>(
+    FragmentPointRecordBinding::bind,R.layout.fragment_point_record
+) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
