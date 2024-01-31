f3cc09eed7f004d703f13529b4c5a08441030636
정철주 <cjfwn96@naver.com>
2024-01-31 18:01:43 +0900

    feat: 최근 위치 호출 후 카메라 이동
    Change-Id: I7e12175489202968c94f4ddcbf4308e0c12cfe8d

--- AD/relpl/presentation/src/main/java/com/gdd/presentation/report/ReportFragment.kt
+++ AD/relpl/presentation/src/main/java/com/gdd/presentation/report/ReportFragment.kt
@@ -10,13 +10,23 @@ import android.util.Log
 import android.view.View
 import androidx.activity.OnBackPressedCallback
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
 import com.gdd.presentation.MainActivity
 import com.gdd.presentation.R
 import com.gdd.presentation.base.BaseFragment
 import com.gdd.presentation.base.PermissionHelper
 import com.gdd.presentation.databinding.FragmentReportBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
 
 private const val TAG = "ReportFragment_Genseong"
 
@@ -26,6 +36,12 @@ class ReportFragment : BaseFragment<FragmentReportBinding>(
     private lateinit var mainActivity: MainActivity
 
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+    private lateinit var naverMap: NaverMap
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -47,8 +63,18 @@ class ReportFragment : BaseFragment<FragmentReportBinding>(
         bottomSheetBehavior = BottomSheetBehavior.from(binding.layoutBottomSheet).apply {
             state = BottomSheetBehavior.STATE_EXPANDED
         }
-
         checkLocationPermission()
+        /**
+         * 이 이후로 위치 권한이 필요한 코드를 작성
+         */
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(mainActivity)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.layout_map) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                childFragmentManager.beginTransaction().add(R.id.layout_map, it).commit()
+            }
+        mapFragment.getMapAsync(mapReadyCallback)
 
         registerListener()
     }
@@ -99,4 +125,17 @@ class ReportFragment : BaseFragment<FragmentReportBinding>(
             }
             .show()
     }
+
+    @SuppressLint("MissingPermission")
+    private val mapReadyCallback = OnMapReadyCallback { map ->
+        naverMap = map
+        naverMap.locationTrackingMode = LocationTrackingMode.NoFollow
+        naverMap.uiSettings.apply {
+            isTiltGesturesEnabled = false
+            isRotateGesturesEnabled = false
+        }
+
+        naverMap.moveCamera(CameraUpdate.scrollTo(LatLng(fusedLocationProviderClient.lastLocation.result)))
+    }
+
 }
