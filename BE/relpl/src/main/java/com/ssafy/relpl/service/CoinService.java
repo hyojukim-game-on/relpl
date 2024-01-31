4562fbb3c2e93f1ef49b966273c1308632ee7222
송민석 <tig04204@naver.com>
2024-01-31 16:43:26 +0900

    feat : coin 로직 모두 완성
    Change-Id: Id63551bf612a8ce614269af38f92c556567bd8df

--- BE/relpl/src/main/java/com/ssafy/relpl/service/CoinService.java
+++ BE/relpl/src/main/java/com/ssafy/relpl/service/CoinService.java
@@ -4,21 +4,16 @@ import com.ssafy.relpl.db.postgre.entity.Coin;
 import com.ssafy.relpl.db.postgre.entity.User;
 import com.ssafy.relpl.db.postgre.repository.CoinRepository;
 import com.ssafy.relpl.db.postgre.repository.UserRepository;
+import com.ssafy.relpl.dto.response.CoinBarcodeResponseDto;
 import com.ssafy.relpl.dto.response.CoinScoreDataResponseDto;
 import com.ssafy.relpl.dto.response.CoinScoreResponseDto;
-import com.ssafy.relpl.service.ResponseService;
+import com.ssafy.relpl.service.result.SingleResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.Random;
-import java.util.stream.Collectors;
+import java.util.*;
+import java.util.Collections;
 
 @Slf4j
 @Service
@@ -29,143 +24,96 @@ public class CoinService {
     private final UserRepository userRepository;
     private final ResponseService responseService;
 
-    // 프로젝트 완료 시 지급하는 100 point coin
-    private static final int PROJECT_COMPLETION_POINTS = 100;
 
-    // 플로깅 프로젝트 100meter 주행 시, 10point coin 씩 지급
-    private static final int PLOGGING_SHORT_POINTS = 10;
+    // 코인 지급 내역 조회 메서드
+    public SingleResult<CoinScoreDataResponseDto> coinScore(Long userId) {
+        log.info("여기는 서비스단이다. 코인 지급 내역을 조회한다.");
 
-    // reportCoordinate의 위도,경도 위치를 한번씩 등록할때마다, 100point coin 씩 차감되는 것.
-    private static final int LOCATION_FLAG_DEDUCTION_POINTS = -100;
-
-
-    // 프로젝트 완료 시 지급하는 100 point coin 지급하는 로직 작성
-    public void completeProject(Long userId) {
-        log.info("프로젝트 완료 시 포인트 {}point 지급", PROJECT_COMPLETION_POINTS);
-        updateUserCoin(userId, PROJECT_COMPLETION_POINTS, generateCoinEventDetail());
-    }
-
-    // 플로깅 프로젝트 100meter 주행 시, 10point coin 씩 지급 로직
-    public void completeShortPlogging(Long userId) {
-        log.info("100미터 플로깅 완료 시 포인트 {}point 지급", PLOGGING_SHORT_POINTS);
-        updateUserCoin(userId, PLOGGING_SHORT_POINTS, generateCoinEventDetail());
-    }
-
-    // reportCoordinate의 위도,경도 위치를 한번씩 등록할때마다, 100point coin 씩 차감되는  로직
-    public void reportLocationFlag(Long userId) {
-        log.info("위치 플래그 제보 시 포인트 {}point 차감", LOCATION_FLAG_DEDUCTION_POINTS);
-        // 여기 포인트 차감 로직을 추가
-        updateUserCoin(userId, LOCATION_FLAG_DEDUCTION_POINTS, generateCoinEventDetail());
-    }
-
-    public Long generateCoinEventId() {
-        // 여기에 동적으로 CoinEventId를 생성하는 로직을 작성하세요.
-        // 예를 들어, 현재 시간을 기반으로 한 값을 사용할 수 있습니다.
-        return System.currentTimeMillis();
-    }
-
-    public String generateCoinEventDate() {
-        // 여기에 동적으로 CoinEventDate를 생성하는 로직을 작성하세요.
-        // 예를 들어, 현재 시간을 기반으로 한 값을 사용할 수 있습니다.
-        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
-    }
-
-    public int generateCoinAmount() {
-        // 여기에 동적으로 CoinAmount를 생성하는 로직을 작성하세요.
-        // 예를 들어, 랜덤 값을 사용할 수 있습니다.
-        return new Random().nextInt(100) + 1; // 1부터 100 사이의 랜덤 값
-    }
-
-    public String generateCoinEventDetail() {
-        // 여기에 동적으로 CoinEventDetail을 생성하는 로직을 작성하세요.
-        // 예를 들어, 랜덤 문자열을 사용할 수 있습니다.
-        int length = 10;
-        return RandomStringUtils.randomAlphanumeric(length);
-    }
-
-    // 유저의 현재 코인 정보 조회 및 업데이트 로직 (update vs select)
-    private void updateUserCoin(Long userId, int coinAmount, String coinEventDetail) {
-        // 사용자의 현재 코인 정보 조회
-        List<Coin> userCoins = coinRepository.findAllByUserId(userId);
-
-        // 사용자의 현재 코인 총합 계산
-        int userTotalCoin = calculateUserTotalCoin(userCoins);
-
-        // 사용자의 현재 코인 정보 업데이트
-        CoinScoreDataResponseDto coinScoreDataResponseDto = new CoinScoreDataResponseDto();
-        coinScoreDataResponseDto.setUserTotalCoin(userTotalCoin);
-
-        // eventList 구성
-        List<CoinScoreResponseDto> eventList = new ArrayList<>();
-
-        for (Coin coin : userCoins) {
-            CoinScoreResponseDto coinScoreResponseDto = new CoinScoreResponseDto();
-            coinScoreResponseDto.setCoinEventDate(coin.getCoinEventDate());
-            coinScoreResponseDto.setCoinAmount(coin.getCoinAmount());
-            coinScoreResponseDto.setCoinEventDetail(coin.getCoinEventDetail());
-            eventList.add(coinScoreResponseDto);
+        // userId 체크
+        if (userId == null) {
+            // userId가 null인 경우 처리
+            return responseService.getFailResult(400, "유저 정보가 없습니다.", new CoinScoreDataResponseDto());
         }
 
-        coinScoreDataResponseDto.setEventList(eventList);
+        // 등록되어 존재하는 유저인지 확인
+        User existingUser = userRepository.findById(String.valueOf(userId)).orElse(null);
 
-        // 사용자의 현재 코인 정보 업데이트 후 저장
-        // 이 부분에서 적절한 로직으로 코인 정보를 업데이트하고 저장하는 부분을 구현해야 합니다.
-         coinRepository.saveAll(userCoins);
+        if (existingUser != null) {
+            // 등록되어 존재하는 유저인 경우
+            // 코인 지급 내역 조회
+            List<Coin> coins = coinRepository.findAllByUserId(userId);
 
-        log.info("사용자 {}에게 {}포인트가 지급되었습니다. 현재 보유한 포인트: {}", userId, coinAmount, userTotalCoin);
+            if (!coins.isEmpty()) {
+                // 총 코인 액수 계산
+                int totalCoin = 0;
+
+                // 응답 데이터 구성
+                CoinScoreDataResponseDto responseDto = new CoinScoreDataResponseDto();
+                List<CoinScoreResponseDto> coinScoreResponseDtos = new ArrayList<>();
+
+                for (Coin coin : coins) {
+                    totalCoin += coin.getCoinAmount();
+
+                    CoinScoreResponseDto coinScoreResponseDto = convertToCoinScoreResponseDto(coin);
+                    coinScoreResponseDtos.add(coinScoreResponseDto);
+                }
+
+                responseDto.setUserTotalCoin(totalCoin);
+                responseDto.setEventList(coinScoreResponseDtos);
+
+                return responseService.getSingleResult(responseDto, "코인 지급 내역 조회 성공", 200);
+            } else {
+                // 코인 지급 내역이 없는 경우
+                return responseService.getFailResult(204, "코인 지급 내역이 없습니다.", new CoinScoreDataResponseDto());
+            }
+        } else {
+            // 등록되지 않은 유저인 경우
+            return responseService.getFailResult(400, "등록되지 않은 유저입니다.", new CoinScoreDataResponseDto());
+        }
     }
 
 
-    // db 삽입 로직
-    public void insertCoinData(Long userId, Long coinEventId, String coinEventDate, int coinAmount, String coinEventDetail) {
-        log.info("포인트 데이터 삽입: userId={}, coinEventId={}, coinEventDate={}, coinAmount={}, coinEventDetail={}",
-                userId, coinEventId, coinEventDate, coinAmount, coinEventDetail);
-
-        Coin coin = Coin.builder()
-                .userId(userId)
-                .coinEventId(coinEventId)
-                .coinEventDate(coinEventDate)
-                .coinAmount(coinAmount)
-                .coinEventDetail(coinEventDetail)
-                .build();
-
-        coinRepository.save(coin);
-
+    // 코인 지급 내역을 응답 DTO로 변환하는 메서드
+    private CoinScoreResponseDto convertToCoinScoreResponseDto(Coin coin) {
+        CoinScoreResponseDto responseDto = new CoinScoreResponseDto();
+        responseDto.setCoinEventDate(coin.getCoinEventDate());
+        responseDto.setCoinAmount(coin.getCoinAmount());
+        responseDto.setCoinEventDetail(coin.getCoinEventDetail());
+        return responseDto;
     }
 
-    // userTotalCoin 만드는 로직
-    public CoinScoreDataResponseDto getCoinScoreData(Long userId) {
-        CoinScoreDataResponseDto coinScoreDataResponseDto = new CoinScoreDataResponseDto();
+    // coinBarcode 조회 메서드
+    public SingleResult<CoinBarcodeResponseDto> coinBarcode(Long userId) {
+        log.info("여기는 서비스단이다. 코인 바코드를 조회한다.");
 
-        // 사용자 모든 코인 내역 가져오기
-        List<Coin> userCoins = coinRepository.findAllByUserId(userId);
-
-        // userTotalCoin 계산
-        int userTotalCoin = calculateUserTotalCoin(userCoins);
-        coinScoreDataResponseDto.setUserTotalCoin(userTotalCoin);
-
-        // eventList 구성
-        List<CoinScoreResponseDto> eventList = new ArrayList<>();
-
-        for (Coin coin : userCoins) {
-            CoinScoreResponseDto coinScoreResponseDto = new CoinScoreResponseDto();
-            coinScoreResponseDto.setCoinEventDate(coin.getCoinEventDate());
-            coinScoreResponseDto.setCoinAmount(coin.getCoinAmount());
-            coinScoreResponseDto.setCoinEventDetail(coin.getCoinEventDetail());
-            eventList.add(coinScoreResponseDto);
+        // userId 체크
+        if (userId == null) {
+            // userId가 null인 경우 처리
+            return responseService.getFailResult(400, "유저 정보가 없습니다.", new CoinBarcodeResponseDto());
         }
 
-        coinScoreDataResponseDto.setEventList(eventList);
+        // 등록되어 존재하는 유저인지 확인
+        User existingUser = userRepository.findById(String.valueOf(userId)).orElse(null);
 
-        return coinScoreDataResponseDto;
-    }
-    private int calculateUserTotalCoin(List<Coin> userCoins) {
-        int userTotalCoin = 0;
+        if (existingUser != null) {
+            // 등록되어 존재하는 유저인 경우
+            // 코인 지급 내역 조회
+            List<Coin> coins = coinRepository.findAllByUserId(userId);
 
-        for (Coin coin : userCoins) {
-            userTotalCoin += coin.getCoinAmount();
+            // 총 코인 액수 계산
+            int totalCoin = 0;
+            for (Coin coin : coins) {
+                totalCoin += coin.getCoinAmount();
+            }
+
+            // 응답 데이터 구성
+            CoinBarcodeResponseDto responseDto = new CoinBarcodeResponseDto();
+            responseDto.setUserTotalCoin(totalCoin);
+
+            return responseService.getSingleResult(responseDto, "바코드 조회 성공", 200);
+        } else {
+            // 등록되지 않은 유저인 경우
+            return responseService.getFailResult(400, "등록되지 않은 유저입니다.", new CoinBarcodeResponseDto());
         }
-
-        return userTotalCoin;
     }
 }
