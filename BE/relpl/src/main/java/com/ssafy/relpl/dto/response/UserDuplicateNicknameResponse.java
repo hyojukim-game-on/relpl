117b31d5696dc27df14cd2365d86c61b745a8949
최재성 <jaisung0410@gmail.com>
2024-02-01 21:45:28 +0900

    feat: 유저 닉네임 중복체크 진행 중
    Change-Id: I9686f70a0272055dd63dd51d19790539b0286582

---
+++ BE/relpl/src/main/java/com/ssafy/relpl/dto/response/UserDuplicateNicknameResponse.java
@@ -0,0 +1,14 @@
+package com.ssafy.relpl.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class UserDuplicateNicknameResponse {
+    private boolean isexist;
+
+    public static createUserDuplicateNicknameResponse()
+}
