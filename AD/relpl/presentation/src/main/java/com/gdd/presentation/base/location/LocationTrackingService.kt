5a55e2971e7270f8ce476682de8a890e5e1bc382
cjfwn96 <cjfwn96@naver.com>
2024-02-04 20:50:23 +0900

    feat: 실시간 위치 추적 기능, 추적 관련 알람 표시 구현
    Change-Id: I99990cc63ae6fd8c4d96b2890f3620d454368b7f

--- AD/relpl/presentation/src/main/java/com/gdd/presentation/base/location/LocationTrackingService.kt
+++ AD/relpl/presentation/src/main/java/com/gdd/presentation/base/location/LocationTrackingService.kt
@@ -1,22 +1,25 @@
 package com.gdd.presentation.base.location
 
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
-import android.content.Context
 import android.content.Intent
+import android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleRegistry
+import com.gdd.presentation.R
 import okhttp3.internal.notify
 
 private const val TAG = "LocationTrackingService_Genseong"
 
 class LocationTrackingService : Service(), LifecycleOwner {
 
+    private lateinit var notificationManager: NotificationManager
     private lateinit var locationProviderController: LocationProviderController
     private val lifecycleRegistry = LifecycleRegistry(this)
     override val lifecycle: Lifecycle
@@ -25,32 +28,42 @@ class LocationTrackingService : Service(), LifecycleOwner {
     override fun onCreate() {
         super.onCreate()
         locationProviderController = LocationProviderController(baseContext, this)
+        makeNoticeChannel()
     }
 
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         super.onStartCommand(intent, flags, startId)
-        makeNoticeChannel()
         val notificationBuilder =
-            Notification
-                .Builder(baseContext, LOCATION_TRANCKING_CHANNEL_ID)
+            NotificationCompat
+                .Builder(this, LOCATION_TRANCKING_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_location)
                 .setContentTitle("플로깅 경로를 기록중입니다.")
                 .setContentText("")
+                .setOnlyAlertOnce(true)
+                .setAutoCancel(false)
                 .setOngoing(true)
+
         val noti = notificationBuilder.build()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            startForeground(LOCATION_TRACKING_SERVICE_ID, noti, FOREGROUND_SERVICE_TYPE_LOCATION)
+        } else {
+            startForeground(LOCATION_TRACKING_SERVICE_ID, noti)
+        }
 
 
         locationProviderController.startTrackingLocation(0) { location, locationTrackingException ->
             if (location != null) {
                 Log.d(TAG, "onStartCommand: ${location.longitude}${location.latitude}")
-                synchronized(notificationBuilder) {
-                    notificationBuilder.setContentText("${location.latitude}\n${location.longitude}")
-                    notificationBuilder.notify()
+                synchronized(notificationManager) {
+                    notificationBuilder.setContentText("${location.longitude} ${location.latitude}")
+                    notificationManager.notify(LOCATION_TRACKING_SERVICE_ID,notificationBuilder.build())
                 }
 
             }
         }
-        startForeground(LOCATION_TRACKING_SERVICE_ID, noti)
+
 
         return START_STICKY
     }
@@ -65,7 +78,7 @@ class LocationTrackingService : Service(), LifecycleOwner {
     }
 
     private fun makeNoticeChannel() {
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.createNotificationChannel(NotificationChannel(
             LOCATION_TRANCKING_CHANNEL_ID,
             "실시간 플로깅 추적 알람",
