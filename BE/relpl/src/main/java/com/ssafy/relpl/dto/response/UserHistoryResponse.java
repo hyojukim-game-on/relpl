5297af33f3150575131588860fed4a4bd2d36f98
김효주 <mok03189@naver.com>
2024-02-06 14:24:49 +0900

    feat: 내 기록 조회하기 완성
    Change-Id: I848a8a84cd7e1297da8b6a2e3486aab71a6a3a3b

--- BE/relpl/src/main/java/com/ssafy/relpl/dto/response/UserHistoryResponse.java
+++ BE/relpl/src/main/java/com/ssafy/relpl/dto/response/UserHistoryResponse.java
@@ -1,13 +1,22 @@
 package com.ssafy.relpl.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+
+@Getter
+@Setter
+@AllArgsConstructor
 @Builder
 public class UserHistoryResponse {
     int totalProject;
     int userTotalDistance;
     int userTotalTime;
-    List<?> detailList;
+    ArrayList<Map<String, Object>> detailList;
 }
