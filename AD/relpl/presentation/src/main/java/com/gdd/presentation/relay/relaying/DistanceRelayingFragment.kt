511d04fda4b785b2f17bc057ee811d47e673e4f4
정철주 <cjfwn96@naver.com>
2024-02-07 13:49:38 +0900

    refactor: 릴레이 진행 프래그먼트 ui 변화 로직 하위 프래그먼트로 이동
    Change-Id: Ice25e77c2180e539625156c8c669b2f0df923bfc

--- AD/relpl/presentation/src/main/java/com/gdd/presentation/relay/relaying/DistanceRelayingFragment.kt
+++ AD/relpl/presentation/src/main/java/com/gdd/presentation/relay/relaying/DistanceRelayingFragment.kt
@@ -7,8 +7,10 @@ import android.view.View
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.lifecycle.viewModelScope
 import com.gdd.presentation.R
 import com.gdd.presentation.base.location.relaying_service.DistanceLocationTrackingService
+import com.gdd.presentation.mapper.DateFormatter
 import com.naver.maps.map.CameraAnimation
 import com.naver.maps.map.CameraPosition
 import com.naver.maps.map.CameraUpdate
@@ -17,21 +19,25 @@ import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.PathOverlay
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 private const val TAG = "DistanceRelayingFragmen_Genseong"
+
 @AndroidEntryPoint
-class DistanceRelayingFragment: RelayingFragment() {
+class DistanceRelayingFragment : RelayingFragment() {
 
     override var naverMap: NaverMap? = null
     private var pathOverlay = PathOverlay()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        with(pathOverlay){
+        with(pathOverlay) {
             color = _activity.getColor(R.color.sage_green)
         }
     }
@@ -46,17 +52,20 @@ class DistanceRelayingFragment: RelayingFragment() {
         // trackingStateFlow
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                relayingViewModel.trackingStateFlow.collect{ list ->
+                relayingViewModel.trackingStateFlow.collect { list ->
                     Log.d(TAG, "registerObserve: trackingStateFlow")
                     Log.d(TAG, "registerObserve: $list")
-                    if(list.isNotEmpty()){
+                    if (list.isNotEmpty()) {
                         naverMap?.let { map ->
                             map.locationOverlay.position = list.last().latLng
-                            map.moveCamera(CameraUpdate.scrollTo(list.last().latLng).animate(CameraAnimation.Linear))
+                            map.moveCamera(
+                                CameraUpdate.scrollTo(list.last().latLng)
+                                    .animate(CameraAnimation.Linear)
+                            )
                         }
-                        if (list.size >= 2){
+                        if (list.size >= 2) {
                             pathOverlay.coords = list.map { it.latLng }
-                            if (pathOverlay.map == null){
+                            if (pathOverlay.map == null) {
                                 pathOverlay.map = naverMap
                             }
                         }
@@ -64,22 +73,67 @@ class DistanceRelayingFragment: RelayingFragment() {
                 }
             }
         }
+        registerUiObserve()
+    }
 
+    private var firstTime: Long? = null
+    private var elapsedTimeFlag = true
+    private fun registerUiObserve() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                relayingViewModel.trackingStateFlow.collect { list ->
+                    Log.d(TAG, "registerUiObserve: $list")
+                    Log.d(TAG, "registerUiObserve: $firstTime")
+                    // 시작 시간
+                    if (list.isNotEmpty() && binding.tvStartTime.text.isNullOrBlank()) {
+                        firstTime = list.first().timeMillis
+                        
+                        binding.tvStartTime.text =
+                            DateFormatter.getRelayStartTimeString(list.first().timeMillis)
+                    }
+                    // 진행 거리
+                    binding.tvProgressDistance.text = list.zipWithNext().sumOf {
+                        it.first.latLng.distanceTo(it.second.latLng)
+                    }.toInt().toString() + "m"
+                }
+            }
+        }
 
+        // 경과 시간
+        viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
+            while (elapsedTimeFlag) {
+                Log.d(TAG, "registerUiObserve: Main")
+                if (firstTime != null) {
+                    Log.d(TAG, "registerUiObserve: Main2")
+                    val hour = (System.currentTimeMillis() - firstTime!!) / 3600000
+                    val minute = (System.currentTimeMillis() - firstTime!!) % 3600000 / 60000
+                    withContext(Dispatchers.Main){
+                        binding.tvElapsedTime.text =
+                            "우리 동네를 위한 ${if (hour < 10) "0" else ""}${hour}시간 ${if (minute < 10) "0" else ""}${minute}분"
+                    }
+                }
+                delay(1000)
+            }
+        }
     }
 
     override fun startRelay() {
-        _activity.startForegroundService(Intent(_activity,DistanceLocationTrackingService::class.java))
+        _activity.startForegroundService(
+            Intent(
+                _activity,
+                DistanceLocationTrackingService::class.java
+            )
+        )
     }
 
     override fun stopRelay() {
-        _activity.stopService(Intent(_activity,DistanceLocationTrackingService::class.java))
+        _activity.stopService(Intent(_activity, DistanceLocationTrackingService::class.java))
         viewLifecycleOwner.lifecycleScope.cancel()
         relayingViewModel.stopTracking()
     }
 
 
-    override val mapReady = OnMapReadyCallback{ map ->
+    override val mapReady = OnMapReadyCallback { map ->
         naverMap = map
         naverMap!!.run {
             uiSettings.apply {
@@ -90,7 +144,9 @@ class DistanceRelayingFragment: RelayingFragment() {
             moveCamera(CameraUpdate.zoomTo(18.0).animate(CameraAnimation.Easing))
 
             addOnCameraIdleListener {
-                moveCamera(CameraUpdate.scrollTo(locationOverlay.position).animate(CameraAnimation.Linear))
+                moveCamera(
+                    CameraUpdate.scrollTo(locationOverlay.position).animate(CameraAnimation.Linear)
+                )
             }
         }
 
--- AD/relpl/presentation/src/main/java/com/gdd/presentation/relay/relaying/RelayingViewModel.kt
+++ AD/relpl/presentation/src/main/java/com/gdd/presentation/relay/relaying/RelayingViewModel.kt
@@ -35,76 +35,12 @@ class RelayingViewModel @Inject constructor(
             SharingStarted.WhileSubscribed(5000),
             1
         )
-//        .stateIn(
-//            viewModelScope,
-//            SharingStarted.WhileSubscribed(5000),
-//            listOf()
-//        )
         .map { list ->
             Log.d(TAG, "Viewmodel: trackingStateFlow")
             list.map {
                 it.toTrackingPoint()
             }
         }
-
-    private var _remainDistance = MutableLiveData<String>()
-    val remainDistance: LiveData<String>
-        get() = _remainDistance
-
-    private var _progressDistance = MutableLiveData<String>()
-    val progressDistance: LiveData<String>
-        get() = _progressDistance
-
-    private var _startTime = MutableLiveData<String>()
-    val startTime: LiveData<String>
-        get() = _startTime
-
-    private var _elapsedTime = MutableLiveData<String>()
-    val elapsedTime: LiveData<String>
-        get() = _elapsedTime
-
-
-    init {
-        collectTrackingDataUI()
-        countProgressTime()
-    }
-
-    private fun collectTrackingDataUI() {
-        viewModelScope.launch(Dispatchers.IO) {
-            getLocationTrackingDataUseCase().map { list ->
-                Log.d(TAG, "Viewmodel: trackingStateFlow")
-                list.map {
-                    it.toTrackingPoint()
-                }
-            }.collect { list ->
-                // 시작 시간
-                if (list.isNotEmpty() && startTime.value == null) {
-                    firstTime = list.first().timeMillis
-                    _startTime.postValue(DateFormatter.getRelayStartTimeString(list.first().timeMillis))
-                }
-                // 진행 거리
-                _progressDistance.postValue(list.zipWithNext().sumOf {
-                    it.first.latLng.distanceTo(it.second.latLng)
-                }.toInt().toString() + "m")
-            }
-        }
-    }
-
-    private fun countProgressTime() {
-        viewModelScope.launch(Dispatchers.IO) {
-            while (elapsedTimeFlag) {
-                // 경과 시간
-                if (firstTime != null) {
-                    val hour = (System.currentTimeMillis() - firstTime!!) / 3600000
-                    val minute = (System.currentTimeMillis() - firstTime!!) % 3600000 / 60000
-                    _elapsedTime.postValue(
-                        "우리 동네를 위한 시간 ${if (hour < 10) "0" else ""}${hour}시간 ${if (minute < 10) "0" else ""}${minute}분"
-                    )
-                }
-                delay(30000)
-            }
-        }
-    }
 
     fun stopTracking() {
         viewModelScope.launch {
