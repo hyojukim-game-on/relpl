56ed09d56d44c0de9bc0886e224445ca3db6eb11
최재성 <jaisung0410@gmail.com>
2024-02-07 09:54:28 +0900

    test: gerrit
    Change-Id: I955f96c153995839f0a594a67ad62573724453b5

--- BE/relpl/src/main/java/com/ssafy/relpl/service/ProjectService.java
+++ BE/relpl/src/main/java/com/ssafy/relpl/service/ProjectService.java
@@ -43,6 +43,8 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
+
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
