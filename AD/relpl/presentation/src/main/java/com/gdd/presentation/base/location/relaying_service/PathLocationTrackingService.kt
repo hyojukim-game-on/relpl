23dd328f07b9de293ed077e02a8f6710bcbe9048
정철주 <cjfwn96@naver.com>
2024-02-07 17:53:33 +0900

    feat: PathLocationTrackingService 기본 구조 작성
    Change-Id: Ida150048cedd543e5d36069f5592524d776d05ab

--- AD/relpl/presentation/src/main/java/com/gdd/presentation/base/location/relaying_service/PathLocationTrackingService.kt
+++ AD/relpl/presentation/src/main/java/com/gdd/presentation/base/location/relaying_service/PathLocationTrackingService.kt
@@ -6,13 +6,11 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.flow.last
 import kotlinx.coroutines.launch
-import kotlin.coroutines.CoroutineContext
 
 @AndroidEntryPoint
 class PathLocationTrackingService : LocationTrackingService() {
-    private var scope = CoroutineScope(Dispatchers.IO)
+    private var ioScope = CoroutineScope(Dispatchers.IO)
 
     override var notiMessage = "경로 기반 플로깅 중 입니다."
     override var distanceStandard = 1
@@ -20,20 +18,18 @@ class PathLocationTrackingService : LocationTrackingService() {
     private var relayPath = listOf<RelayPathData>()
 
     override var locationUpdateListener = { location: Location ->
-    }
+        if (relayPath.isNotEmpty()){
 
-    override fun onCreate() {
-        super.onCreate()
-        scope.launch {
-            relayPath
         }
     }
 
     override fun startTracking() {
-
+        ioScope.launch {
+            relayPath = locationTrackingRepository.getAllRelayPathDataOnce()
+        }
     }
 
     override fun stopTracking() {
-        scope.cancel()
+        ioScope.cancel()
     }
 }
