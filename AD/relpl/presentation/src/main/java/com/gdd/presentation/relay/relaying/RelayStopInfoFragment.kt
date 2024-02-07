6a4c6fe0aa64cfcefbba3acbdcc48769d344c0ab
정철주 <cjfwn96@naver.com>
2024-02-07 18:00:45 +0900

    create: RelayStopInfoViewModel
    Change-Id: I37252d9ec60e4f9fa21acbd4e76c74a11424e008

--- AD/relpl/presentation/src/main/java/com/gdd/presentation/relay/relaying/RelayStopInfoFragment.kt
+++ AD/relpl/presentation/src/main/java/com/gdd/presentation/relay/relaying/RelayStopInfoFragment.kt
@@ -3,7 +3,9 @@ package com.gdd.presentation.relay.relaying
 import com.gdd.presentation.R
 import com.gdd.presentation.base.BaseFragment
 import com.gdd.presentation.databinding.FragmentRelayStopInfoBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RelayStopInfoFragment : BaseFragment<FragmentRelayStopInfoBinding>(
     FragmentRelayStopInfoBinding::bind, R.layout.fragment_relay_stop_info
 ) {
