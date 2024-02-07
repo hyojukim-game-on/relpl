4d516cee69a126fd573a47489230573b3ccebab8
정철주 <cjfwn96@naver.com>
2024-02-07 17:08:03 +0900

    design: 릴레이 중단 정보 프래그먼트 ui 구현
    Change-Id: I4150f690f75155c8e09317a78afdaf31250b8ccc

---
+++ AD/relpl/presentation/src/main/java/com/gdd/presentation/relay/relaying/RelayStopInfoFragment.kt
@@ -0,0 +1,11 @@
+package com.gdd.presentation.relay.relaying
+
+import com.gdd.presentation.R
+import com.gdd.presentation.base.BaseFragment
+import com.gdd.presentation.databinding.FragmentRelayStopInfoBinding
+
+class RelayStopInfoFragment : BaseFragment<FragmentRelayStopInfoBinding>(
+    FragmentRelayStopInfoBinding::bind, R.layout.fragment_relay_stop_info
+) {
+
+}
