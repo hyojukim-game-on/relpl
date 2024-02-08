6594be0ee3307206810d7dcc0b027b5984912195
최재성 <jaisung0410@gmail.com>
2024-02-08 10:54:23 +0900

    refactor: fcm_key name 변경
    Change-Id: I1d3b231c1a9cd2de1875553ad8ceb6cea4ffcf3a

--- BE/relpl/src/main/java/com/ssafy/relpl/config/FCMConfig.java
+++ BE/relpl/src/main/java/com/ssafy/relpl/config/FCMConfig.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class FCMConfig {
     @Bean
     FirebaseMessaging firebaseMessaging() throws IOException {
-        ClassPathResource resource = new ClassPathResource("firebase/relpl-31865-firebase-adminsdk-b7su8-3ab2578f6e.json");
+        ClassPathResource resource = new ClassPathResource("firebase/firebase_service_key.json");
 
         InputStream refreshToken = resource.getInputStream();
 
