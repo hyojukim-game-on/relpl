1171381a89d1959b3c3193c18bf0c971978ee5fc
cjfwn96 <cjfwn96@naver.com>
2024-02-13 00:25:10 +0900

    feat: BaseFragment 라이프 사이클 확인 로그 작성
    Change-Id: I3aca920f7f4dbba58fbaa9ed1d06f143930e4650

--- AD/relpl/presentation/src/main/java/com/gdd/presentation/base/BaseFragment.kt
+++ AD/relpl/presentation/src/main/java/com/gdd/presentation/base/BaseFragment.kt
@@ -11,8 +11,14 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import androidx.viewbinding.ViewBinding
 import com.google.android.material.snackbar.Snackbar
+import kotlin.reflect.jvm.jvmName
 
 abstract class BaseFragment<B : ViewBinding>(
     private val bind: (View) -> B,
@@ -55,6 +61,44 @@ abstract class BaseFragment<B : ViewBinding>(
 
     fun showSnackBar(message: String){
         Snackbar.make(binding.root,message,Snackbar.LENGTH_SHORT).show()
+    }
+
+    /**
+     * life cycle check
+     */
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("LIFECYCLE", "${_binding?.javaClass} : CREATE")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        lifecycleObserve.onStateChanged(viewLifecycleOwner,Lifecycle.Event.ON_START)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        lifecycleObserve.onStateChanged(viewLifecycleOwner,Lifecycle.Event.ON_RESUME)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        lifecycleObserve.onStateChanged(viewLifecycleOwner,Lifecycle.Event.ON_PAUSE)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        lifecycleObserve.onStateChanged(viewLifecycleOwner,Lifecycle.Event.ON_STOP)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("LIFECYCLE", "${_binding?.javaClass} : Destory")
+    }
+
+    private val lifecycleObserve = LifecycleEventObserver { source, event ->
+        Log.d("LIFECYCLE", "${_binding?.javaClass} : $event")
     }
 
 
