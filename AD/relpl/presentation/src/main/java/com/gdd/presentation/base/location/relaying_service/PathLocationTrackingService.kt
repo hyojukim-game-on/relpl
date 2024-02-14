08daad5562a0289f1bac3a95cd71f80114278a91
정철주 <cjfwn96@naver.com>
2024-02-14 16:41:08 +0900

    feat: 경로 이탈 시 경고 알람 표출
    Change-Id: Iddf3027e584a90e0e40ccef7f16e5712a2114ae4

--- AD/relpl/presentation/src/main/java/com/gdd/presentation/base/location/relaying_service/PathLocationTrackingService.kt
+++ AD/relpl/presentation/src/main/java/com/gdd/presentation/base/location/relaying_service/PathLocationTrackingService.kt
@@ -1,6 +1,9 @@
 package com.gdd.presentation.base.location.relaying_service
 
+import android.app.Notification
 import android.location.Location
+import androidx.core.app.NotificationCompat
+import com.gdd.presentation.R
 import com.gdd.presentation.model.RelayPath
 import com.gdd.presentation.model.mapper.toRelayPath
 import com.gdd.presentation.model.mapper.toRelayPathData
@@ -10,6 +13,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
+import kotlin.math.sqrt
 
 @AndroidEntryPoint
 class PathLocationTrackingService : LocationTrackingService() {
@@ -21,16 +25,25 @@ class PathLocationTrackingService : LocationTrackingService() {
     private var relayPath = mutableListOf<RelayPath>()
     private var beforePathPoint: RelayPath? = null
 
+    private lateinit var leavePathNoti: NotificationCompat.Builder
+
     override var locationUpdateListener = { location: Location ->
         if (relayPath.isNotEmpty()) {
             // 다음 정점에 가까워 지면 방문 체크
             if (relayPath.first().latLng.distanceTo(LatLng(location)) < 10) {
+                beforePathPoint = relayPath.first() //경로 이탈 확인을 위한 이전 방문 노드 저장
                 ioScope.launch {
                     checkVisit(relayPath.removeFirst())
                 }
             }
-            // TODO 경로 이탈 경고 로직
-            
+            // 경로 이탈 경고 로직
+            if(beforePathPoint != null){
+                if (calculateDistanceToLine(beforePathPoint!!.latLng,relayPath.first().latLng,LatLng(location)) > 20){
+                    notificationManager.notify(PATH_LEAVE_NOTI_ID,leavePathNoti.build())
+                } else {
+                    notificationManager.cancel(PATH_LEAVE_NOTI_ID)
+                }
+            }
         }
     }
 
@@ -43,6 +56,7 @@ class PathLocationTrackingService : LocationTrackingService() {
                     .map { it.toRelayPath() }
                     .toMutableList()
         }
+        initNoti()
     }
 
     override fun stopTracking() {
@@ -53,5 +67,28 @@ class PathLocationTrackingService : LocationTrackingService() {
         locationTrackingRepository.updateRelayPathPoint(
             relayPathPoint.copy(myVisit = true).toRelayPathData()
         )
+    }
+
+    private fun initNoti(){
+        leavePathNoti = NotificationCompat
+            .Builder(this, LOCATION_TRANCKING_CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_warning)
+            .setContentTitle("경로를 벗어났어요!")
+            .setContentText("경로를 벗어났습니다. 플로깅이 기록되지 않아요 원래 위치로 돌아가주세요.")
+            .setAutoCancel(false)
+            .setOngoing(true)
+    }
+
+    private fun calculateDistanceToLine(marker1: LatLng, marker2: LatLng, myPosition: LatLng): Double {
+        val a = marker1.distanceTo(myPosition)
+        val b = marker2.distanceTo(myPosition)
+        val c = marker1.distanceTo(marker2)
+        val s = (a+b+c) / 2.0
+
+        return (2* sqrt(s*(s-a)*(s-b)*(s-c))) / c
+    }
+
+    companion object{
+        const val PATH_LEAVE_NOTI_ID = 100001
     }
 }
