563fe346cb1a153097b1dc9f0d6299956aed1538
정철주 <cjfwn96@naver.com>
2024-02-14 12:28:24 +0900

    test: 경로 이탈 거리 특정 로직 테스트
    Change-Id: Ia63cdc9cd9f86ce8dd03e3dec566adaadf92ffae

--- AD/relpl/presentation/src/test/java/com/gdd/presentation/ExampleUnitTest.kt
+++ AD/relpl/presentation/src/test/java/com/gdd/presentation/ExampleUnitTest.kt
@@ -1,20 +1,17 @@
 package com.gdd.presentation
 
+import com.naver.maps.geometry.LatLng
 import org.junit.Test
 
 import org.junit.Assert.*
+import kotlin.math.sqrt
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
-    }
-
+class UnitTest {
     @Test
     fun phoneFormat(){
         val origin = "01012345678"
@@ -22,4 +19,27 @@ class ExampleUnitTest {
         val format = "${origin.substring(0, 3)} ${origin.substring(3,7)} ${origin.substring(7,11)}"
         assertEquals(format, formatted)
     }
+
+    @Test
+    fun distanceTest(){
+        // 36.102858, 128.419107
+        // 36.102858, 128.420107
+        // 36.103121, 128.420521
+
+        val point1 = LatLng(36.102858, 128.419107)
+        val point2 = LatLng(36.102858, 128.420107)
+        val target = LatLng(36.103121, 128.420521)
+
+        println(calculateDistanceToLine(point1,point2,target))
+    }
+
+    fun calculateDistanceToLine(marker1: LatLng, marker2: LatLng, myPosition: LatLng): Double {
+        val a = marker1.distanceTo(myPosition)
+        val b = marker2.distanceTo(myPosition)
+        val c = marker1.distanceTo(marker2)
+        val s = (a+b+c) / 2.0
+
+        return (2* sqrt(s*(s-a)*(s-b)*(s-c))) / c
+    }
+
 }
